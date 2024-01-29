1e9b6575fd5a4bde3f7fb0c3e5c74bce761728d6
thadtastic <leothaddeus92@gmail.com>
2024-01-29 17:09:04 -0600

    second constructor for location.java created

--- src/main/java/org/launchcode/techjobs/oo/Location.java
+++ src/main/java/org/launchcode/techjobs/oo/Location.java
@@ -14,7 +14,11 @@ public class Location {
     }
 
     // TODO: Add a constructor that takes a string as a parameter and assigns it to the 'value' field. The
+    public Location(String value){
     //  constructor should also call the empty constructor in order to initialize the 'id' field.
+        this();
+        this.value = value;
+    }
 
 
     // Custom toString, equals, and hashCode methods:
